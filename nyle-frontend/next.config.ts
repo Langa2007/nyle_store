22f32a7874cde758b473354dd562af65a2abe7b2
Langa2007 <fidellanga67@gmail.com>
2025-10-20 17:20:03 +0300

    change in nextconfig

--- nyle-frontend/next.config.ts
+++ nyle-frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
 
   // ✅ SWC minification is now automatic — no need to define it manually.
   // ✅ You can safely add experimental keys if needed later.
