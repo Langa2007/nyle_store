2f04005dba5e511ca8d1227a339a4c37050bdeea
Langa2007 <fidellanga67@gmail.com>
2025-12-06 10:05:29 +0300

    changes in wrapper

--- nyle-frontend/src/app/checkout/page.js
+++ nyle-frontend/src/app/checkout/page.js
@@ -1,10 +1,10 @@
 "use client";
 
-import { Suspense} from "react";
+import { Suspense } from "react";
 import Checkout from "./Checkout";
 
-// Disable static generation for this page
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
+
 export default function CheckoutPage() {
   return (
     <Suspense fallback={<p className="text-center mt-10 text-blue-600">Loading checkout...</p>}>
