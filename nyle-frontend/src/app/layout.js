def5a122c10b3e8f4389ab53bc7e90780bb51157
Langa2007 <fidellanga67@gmail.com>
2025-12-17 15:06:08 +0300

    removal of navbar in layout

--- nyle-frontend/src/app/layout.js
+++ nyle-frontend/src/app/layout.js
@@ -1,13 +1,12 @@
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Navbar from "../components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="overflow-x-hidden">
-        <Navbar />
-        <main className="container mx-auto px-4 sm:px-6 lg:px-8 pt-20">
+        {/* Navbar is now included within each page component */}
+        <main className="container mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </main>
       </body>
